9f398e531e999fb50a3ddc057ff17b271694aff3
Hassaan Junaid <hassaanjunaid.soomro@gmail.com>
2022-09-07 14:47:35 -0500

    commit

--- lib/screens/packages/packagecontroller.dart
+++ lib/screens/packages/packagecontroller.dart
@@ -2,8 +2,10 @@ import 'package:get/get.dart';
 import 'package:task2/fakedata/packagesdata.dart';
 
 class PackageController extends GetxController {
+
   var isloading = false.obs;
   List<PackageProduct> mylist = [];
+  
   fetch() async {
     isloading.value = false;
     await Future.delayed(const Duration(seconds: 3));
